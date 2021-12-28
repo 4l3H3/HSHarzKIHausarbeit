f73345446829c3ea53669d0b83ace18fda73d2f6
alehe <65286741+4l3H3@users.noreply.github.com>
2021-12-28 10:23:52 +0100

    Add files via upload

---
+++ Perceptron.py
@@ -0,0 +1,57 @@
+from numpy import dot, array, zeros
+from random import choice
+
+class Perceptron:
+    
+    def __init__(self, iterations = 20, training_data_set = []) -> None:
+        self.iterations = iterations
+        self.training_data_set = training_data_set
+    
+    def major(self, x):
+        # if the dot product is greater than the total amount of inputs, return 1
+        if x > len(self.training_data_set[0][0]) / 2:
+            return 1
+        else:
+            return 0
+    
+    def fit(self, training_data_set, w):       
+        for i in range(self.iterations):
+            
+            # choose random training data
+            training_data = choice(training_data_set)
+            x = training_data[0]
+            y = training_data[1]
+            
+            # calculate dot product 
+            y_hat = self.major(dot(w,x))
+            
+            # calculate error
+            error = y - y_hat
+            
+            print("Iteration:{}, X: {}, Weights: {}, y: {}, Predict:{}, Error:{}".format(i + 1, x, w, y, self.major(dot(x,w)), error))
+            # calculate new weights based in error
+            w += error * x
+            
+
+
+def main():
+    training_data_set = [
+        (array([0,0,0]), 0),
+        (array([0,0,1]), 0),
+        (array([0,1,0]), 0),
+        (array([0,1,1]), 1),
+        (array([1,0,0]), 0),
+        (array([1,0,1]), 1),
+        (array([1,1,0]), 1),
+        (array([1,1,1]), 1),
+    ]    
+    w = zeros(3) # [0.,0.,0.]
+    iterations = 30
+
+    p = Perceptron(iterations=iterations, training_data_set=training_data_set)
+    p.fit(training_data_set, w)
+
+
+
+
+main()
