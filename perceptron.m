5702a3cf2abb9292d4bf5f0c4eda16d818c8fabf
alehe <65286741+4l3H3@users.noreply.github.com>
2022-01-02 14:02:15 +0100

    Add files via upload

---
+++ perceptron.m
@@ -0,0 +1,27 @@
+X = [0 0 0; 0 0 1; 0 1 0; 0 1 1; 1 0 0; 1 0 1; 1 1 0; 1 1 1];
+y = [0 0 0 1 0 1 1 1];
+
+function y = major(value)
+  if (value > 3/2)
+    y = 1;
+  else
+    y = 0;
+  endif
+endfunction
+
+function w = fit(X, y, loops = 30)
+  w = zeros(1,3);
+  for index = 1:loops
+    random = randi(8);
+    x = X(random, :);
+    expected = y(random);
+    hypothesis = major(dot(w,x));
+    error = expected - hypothesis;
+    w = w + error * x;
+  endfor
+endfunction
+
+
+
+final_weights = fit(X,y);
+disp(final_weights);
