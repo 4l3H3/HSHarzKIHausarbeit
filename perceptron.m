fda90c82310efa5d38da1b18fd49025a9dfc3fe7
4l3H3 <65286741+4l3H3@users.noreply.github.com>
2022-02-04 16:33:41 +0100

    added comments, changed fit loop to loop through every input once

--- perceptron.m
+++ perceptron.m
@@ -1,20 +1,37 @@
 X = [0 0 0 1; 0 0 1 1; 0 1 0 1; 0 1 1 1; 1 0 0 1; 1 0 1 1; 1 1 0 1; 1 1 1 1];
 y = [0 0 0 1 0 1 1 1];
 
+# sigmoid activation function
 function y = sigmoid(x)
   y = 1 / (1 + exp(-x));
 endfunction
 
-function w = fit(X, y, loops = 100, learnrate = 0.2)
+# step function
+function y = heaviside(x)
+  if (x < 0)
+    y = 0;
+  else 
+    y = 1;
+  endif
+endfunction
+
+# fitting function ; X = input, y = expected output, returns weights
+function w = fit(X, y, loops = 100, learningrate = 1)
+  # initialize weights based on input length
   length = columns(X);
   w = zeros(1,length);
-  for index = 1:loops
-    random = randi(8);
-    x = X(random, :);
-    expected = y(random);
+  
+  # loop through the inputs
+  for index = 1:rows(X)
+    # pick current input and expected output
+    x = X(index, :); 
+    expected = y(index);
+    # predict the output based on current weights
     hypothesis = sigmoid(dot(w,x));
+    # calculate error
     error = expected - hypothesis;
-    w += learnrate * error * x;
+    # perceptron learing rule
+    w += learningrate * error * x;
   endfor
 endfunction
 
